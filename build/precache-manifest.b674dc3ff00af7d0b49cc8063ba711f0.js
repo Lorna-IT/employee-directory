d3dfad22272d8fdd40532947e2949a2a52b3aeee
Lorna Lynch <lornalynchit@yahoo.com>
2020-04-14 20:10:44 -0400

    added build

---
+++ build/precache-manifest.b674dc3ff00af7d0b49cc8063ba711f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf83854c8f04b3f4a9f0ee1a95d6a8e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "132ae9f9fdea4a578dfc",
+    "url": "/static/css/main.16f2466d.chunk.css"
+  },
+  {
+    "revision": "002c73850a56e44212bf",
+    "url": "/static/js/2.e3a9c51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3a9c51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132ae9f9fdea4a578dfc",
+    "url": "/static/js/main.e5dac1d0.chunk.js"
+  },
+  {
+    "revision": "23f6ac9b485ef64d3c6e",
+    "url": "/static/js/runtime-main.199e206c.js"
+  }
+]);
